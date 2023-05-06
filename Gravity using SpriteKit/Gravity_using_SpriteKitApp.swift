af3881bb9b494be543d01d3509bd9729015855ea
Asadbek <asadbekn443@gmail.com>
2023-05-06 00:58:27 -0400

    Initial Commit

---
+++ Gravity using SpriteKit/Gravity_using_SpriteKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gravity_using_SpriteKitApp.swift
+//  Gravity using SpriteKit
+//
+//  Created by Asadbek Nematov on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Gravity_using_SpriteKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
